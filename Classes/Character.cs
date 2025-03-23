7bb5ab2c76c2a62895fdf84330dc1283fb4dc1d2
Fresh <kizzak1111@gmail.com>
2025-03-23 22:04:55 +0100

    Fixed folder structure.

--- Classes/Character.cs
+++ Classes/Character.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InvincibleGame
+namespace InvincibleGame.Classes
 {
     public class Character
     {
--- Forms/GameBoard.cs
+++ Forms/GameBoard.cs
@@ -1,4 +1,5 @@
 using System.Media;
+using InvincibleGame.Classes;
 
 
 
