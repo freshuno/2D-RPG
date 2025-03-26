ff55d2e4a99d1d86cf38c1f23f701a70d55eec6b
freshuno <kizzak1111@gmail.com>
2025-03-26 20:54:17 +0100

    Clean up.

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace Rpg2D.Properties {
         /// <summary>
         ///   Wyszukuje zlokalizowany zasób typu System.Byte[].
         /// </summary>
+        internal static byte[] gameIcon {
+            get {
+                object obj = ResourceManager.GetObject("gameIcon", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Wyszukuje zlokalizowany zasób typu System.Byte[].
+        /// </summary>
         internal static byte[] heroMove {
             get {
                 object obj = ResourceManager.GetObject("heroMove", resourceCulture);
@@ -96,16 +106,6 @@ namespace Rpg2D.Properties {
         internal static byte[] mainMenuMusic {
             get {
                 object obj = ResourceManager.GetObject("mainMenuMusic", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Wyszukuje zlokalizowany zasób typu System.Byte[].
-        /// </summary>
-        internal static byte[] skrzyżowane_miecze_w_pikselowym_stylu__ikona_programu_removebg_preview {
-            get {
-                object obj = ResourceManager.GetObject("skrzyżowane_miecze_w_pikselowym_stylu__ikona_programu-removebg-preview", resourceCulture);
                 return ((byte[])(obj));
             }
         }
